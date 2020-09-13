60c9ff0046b9b06b905a83ae0f4263926d9b10fe
lochloch <lochana.ahire17@gmail.com>
2020-09-13 00:59:24 -0500

    completed angular routing list app

--- app.js
+++ app.js
@@ -1,97 +1,87 @@
 var app = angular.module("myApp", ["ngRoute"]);
 
+// Create the products array through angular factory to share data between multiple controllers
+app.factory('AppData', function () {
+    return {
+        products: [{ title: 'Milk', desc: 'white' }]
+    };
+});
+
+
 // Routing
-app.config(function($routeProvider){
+app.config(function ($routeProvider) {
     $routeProvider
 
-            // route for the home page
-            .when('/', {
-                templateUrl : '/home.html',
-                controller  : 'mainController'
-            })
+        // route for the home page
+        .when('/', {
+            templateUrl: '/home.html',
+            controller: 'mainController'
+        })
 
-            // route for the about page
-            .when('/about', {
-                templateUrl : '/about.html',
-                controller  : 'aboutController'
-            })
+        // route for the about page
+        .when('/:id', {
+            templateUrl: '/about.html',
+            controller: 'aboutController'
+        })
 
-            .otherwise({redirectTo: '/'});
+        .otherwise({ redirectTo: '/' });
 
 });
 
 // create the controller and inject Angular's $scope
 
 // Main Controller
-app.controller('mainController', function($scope) {
+app.controller('mainController', function ($scope, $location, AppData) {
 
-    
+
     // create a message to display in our view
     $scope.message = 'Product List App';
 
     // Add product to list
-    $scope.product = { title: "", desc: ""};
-    $scope.products = [
-        {title: 'Milk', desc: 'white'},
-        {title: 'Coco', desc: 'Brown'}
-    ];
-    $scope.cur = { title: "", desc: ""};
-    $scope.showCur = false;
+    $scope.product = { title: "", desc: "" };
+    $scope.products = AppData.products;
+
     $scope.addItem = function () {
         $scope.products.push($scope.product);
-        $scope.product = { title: "", desc: ""};
+        $scope.product = { title: "", desc: "" };
         $scope.showCur = false;
-    } 
+    }
 
     // Remove product from list 
     $scope.removeItem = function (x) {
-        $scope.products.splice(x, 1); 
-    }  
+        $scope.products.splice(x, 1);
+    }
 
     // Display particular product-description
     $scope.displayItem = function (x) {
-        $scope.cur = $scope.products[x];
-        $scope.showCur = true;
-    }  
+        $location.path("/" + x);
+    }
 
-    // back button result
-    $scope.backDisp = function() {
-        $scope.showCur = false;
-    } 
+});
 
-    });
 //--------------------------------------------------------------------------//
-    // About COntroller
-    app.controller('aboutController', function($scope) {
+
+// About Controller
+app.controller('aboutController', function ($scope, $location, AppData) {
 
     // create a message to display in our view
     $scope.message = 'Product List App';
 
+    $scope.products = AppData.products;
+    const id = parseInt($location.url().substring(1));
+    $scope.cur = $scope.products[id];
+
     // Add product to list
-    $scope.product = { title: "", desc: ""};
-    $scope.cur = { title: "", desc: ""};
-    $scope.showCur = false;
+    $scope.product = { title: "", desc: "" };
     $scope.addItem = function () {
         $scope.products.push($scope.product);
-        $scope.product = { title: "", desc: ""};
-        $scope.showCur = false;
-    } 
-
-    // Remove product from list 
-    $scope.removeItem = function (x) {
-        $scope.products.splice(x, 1); 
-    }  
-
-    // Display particular product-description
-    $scope.displayItem = function (x) {
-        $scope.cur = $scope.products[x];
-        $scope.showCur = true;
-    }  
+        $location.path("/");
+    }
 
     // back button result
-    $scope.backDisp = function() {
-        $scope.showCur = false;
-    } 
+    $scope.backDisp = function () {
+        $location.path("/");
+    }
 
-    });
+});
 
